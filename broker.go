84e6d49ae8232ac4d93d2d94912c64704bdd4bfc
Alvin Tsao <alvin.yw.tsao@appier.com>
2020-03-25 16:06:39 +0800

    Modify: update

--- broker.go
+++ broker.go
@@ -208,7 +208,7 @@ func StartBroker(p string) {
 	}()
 
 	go func() {
-		fmt.Println("broker.main: Start serving")
+		fmt.Println("broker.main: Starts server")
 		if err := hs.ListenAndServe(); err != http.ErrServerClosed {
 			fmt.Printf("broker.main: Failed to serve: %v\n", err)
 		}
